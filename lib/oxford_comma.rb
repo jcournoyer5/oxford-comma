1100b44e330af7432404ff3a5db81c6d773ff99d
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-16 03:27:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
     array.join
+    if array.count == 2
+       array << "and"
     
-end
+    elsif array.count == 3
+        array.split << "and"
+            
+    else array.count > 3
+            array.split
+    
+    end
+end 
