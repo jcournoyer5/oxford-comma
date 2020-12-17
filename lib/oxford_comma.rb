d8b6af90a2c1e722391a1421f14f711cafb7f2e9
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-17 23:25:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,8 @@
 def oxford_comma(array)
-    array.join
-    if array.count == 2
-       array << "and"
-    
-    elsif array.count == 3
-        array.split << "and"
-            
-    else array.count > 3
-            array.split
-    
-    end
-end 
+    if array.length == 2 
+ return   "#{array.first} and #{array.last}"
+    elsif array.length > 2
+    array[-1].insert(0, "and ")
+    end 
+  array.join(", ")
+end
